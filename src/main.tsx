bda97f695ddbffe9d0bbb73f80a34f4171f2329e
nousantx <nousantx@gmail.com>
2024-07-01 08:15:39 +0700

    style: add nosant-icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Routes } from "@generouted/react-router";
+import "nosant-icon";
 import "./style/css/index.css";
 import "./style/css/color.css";
 import "./style/css/google-icons.css";

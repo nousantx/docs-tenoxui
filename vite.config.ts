b0af640c7ca3237ce0d53c68662f64ce62690220
nousantx <nousantx@gmail.com>
2024-07-01 08:14:16 +0700

    chore: add mdx plugin for docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-// vite.config.ts
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import generouted from "@generouted/react-router/plugin";
+import mdx from "@mdx-js/rollup";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [react(), generouted()],
-  resolve: { alias: { "@": "/src" } }
+  plugins: [{ enforce: "pre", ...mdx() }, react(), generouted()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@style": path.resolve(__dirname, "./src/style"),
+      "@component": path.resolve(__dirname, "./src/components"),
+      "@styler": path.resolve(__dirname, "./src/style/styler.ts")
+    }
+  }
 });
